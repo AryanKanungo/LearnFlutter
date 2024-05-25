d9972a68960b338a8daddb419cdde8dd80b7b6ea
AryanKanungo <aryan.kanungo@somaiya.edu>
2024-05-25 16:47:50 +0530

    day5comp

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:first/pages/Home.dart';
 import 'package:first/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.cyan),
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      //initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => Home(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,13 +6,53 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 25, color: Colors.blue, fontWeight: FontWeight.bold),
+        child: Column(
+      children: [
+        Image.asset(
+          "assets/images/loginimg.png",
+          fit: BoxFit.cover,
         ),
-      ),
-    );
+        Text(
+          "Welcome",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 0.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter Username",
+                  labelText: "Username",
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print("MESSIII");
+                },
+                child: Text("Login"),
+                style: TextButton.styleFrom(),
+              )
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
